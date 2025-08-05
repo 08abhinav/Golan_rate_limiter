ad015e898d1a83f5d329293b605535b9d09ce217
08abhinav <abhinavnegi101@gmail.com>
2025-08-05 20:17:54 +0530

    implementation done

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type TokenBucket struct {
 	tokens       int
@@ -9,28 +12,47 @@ type TokenBucket struct {
 	lastRefillTs time.Time
 }
 
-func min(a, b int)int{
-	if a < b{
+func min(a, b int) int {
+	if a < b {
 		return a
 	}
 	return b
 }
 
-/*This method will check if a request can be allowed and also 
+/*This method will check if a request can be allowed and also
 calculate how many tokens to add */
-func (b *TokenBucket) AllowRequest() bool{
+func (b *TokenBucket) AllowRequest() bool {
 	now := time.Now()
 	elapsed := now.Sub(b.lastRefillTs).Seconds()
 
 	tokensToAdd := int(elapsed * float64(b.refillRate))
-	if tokensToAdd > 0{
-		b.tokens = min(b.capacity, b.tokens + tokensToAdd)
+	if tokensToAdd > 0 {
+		b.tokens = min(b.capacity, b.tokens+tokensToAdd)
 		b.lastRefillTs = now
 	}
 
-	if b.tokens > 0{
+	if b.tokens > 0 {
 		b.tokens--
 		return true
 	}
 	return false
+}
+
+func main() {
+	bucket := &TokenBucket{
+		tokens:       5,
+		capacity:     3,
+		refillRate:   2,
+		lastRefillTs: time.Now(),
+	}
+
+	for i := 1; i <= 10; i++{
+		allowed := bucket.AllowRequest()
+
+		if allowed{
+			fmt.Printf("Request %d allowed at %s\n", i, time.Now().Format("15:04:05.000"))
+		}else{
+			fmt.Printf("Request %d blocked at %s\n", i, time.Now().Format("15:04:05.000"))
+		}
+	}
 }
